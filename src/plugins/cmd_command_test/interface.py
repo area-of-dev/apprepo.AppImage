9ac0daab82274169852770d02461ed47f2e95f53
Alex Woroschilow <alex.woroschilow@gmail.com>
2020-11-21 20:02:39 +0100

    =bugfix

--- src/plugins/cmd_command_test/interface.py
+++ src/plugins/cmd_command_test/interface.py
@@ -10,10 +10,10 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 import os
-import pty
 import signal
 import stat
 import subprocess
+import tempfile
 import time
 
 import hexdi
@@ -21,29 +21,28 @@ import psutil
 
 
 @hexdi.inject('console')
-def _check(appimage, console):
-    yield "[{}]: starting subprocess {}...".format(console.blue('testing'), appimage)
+def _test_appimage(appimage, console):
+    with tempfile.TemporaryFile() as stderr:
+        process = subprocess.Popen(appimage, stderr=stderr, stdout=stderr, preexec_fn=os.setsid)
+        yield console.comment("[testing]: starting subprocess {}...".format(appimage))
 
-    err_r, out_r = pty.openpty()
-    process = subprocess.Popen(appimage, stderr=err_r, stdout=err_r, preexec_fn=os.setsid)
+        time.sleep(5)
 
-    time.sleep(5)
-
-    while not process.poll():
         yield console.comment("[testing]: stopping subprocess {}...".format(appimage))
-        if not psutil.pid_exists(process.pid):
-            break
+        if psutil.pid_exists(process.pid):
+            os.killpg(process.pid, signal.SIGTERM)
 
-        os.killpg(process.pid, signal.SIGTERM)
-        break
+        stderr.seek(0)
 
-    output = str(os.read(err_r, 1024 * 1024), 'utf-8', errors='ignore')
-    yield console.warning("[testing]: status {}, stderr: {}...".format(process.returncode, output))
-    os.remove(appimage)
+        output = str(stderr.read(), 'utf-8', errors='ignore')
+        yield console.warning("[testing]: status {}, stderr: {}...".format(process.returncode, output))
+
+        os.remove(appimage)
+        stderr.close()
 
 
-@hexdi.inject('appimagetool', 'apprepo', 'downloader', 'console')
-def _test_action(search=None, options=None, appimagetool=None, apprepo=None, downloader=None, console=None):
+@hexdi.inject('apprepo', 'downloader', 'console')
+def _test_search_request_element(search=None, options=None, apprepo=None, downloader=None, console=None):
     yield console.comment("[processing]: search request {}...".format(search))
 
     collection = apprepo.search('') \
@@ -66,8 +65,8 @@ def _test_action(search=None, options=None, appimagetool=None, apprepo=None, dow
         assert (os.path.exists(download))
 
         os.chmod(download, stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP | stat.S_IRWXO | stat.S_IROTH)
-        for output in _check(download):
-            yield output
+        for line in _test_appimage(download):
+            yield line
 
 
 console = hexdi.resolve('console')
@@ -76,12 +75,12 @@ description = "Test compatibility of the selected package with the current host"
 
 
 @console.task(name=['test', 'check', 'validate'], description=description)
-def test_action(options=None, args=None):
+def test_search_request(options=None, args=None):
     if args is None or not len(args):
         args = [None]
 
     for package in args:
         package = package.strip('\'"') if package else None
-        for output in _test_action(package, options):
+        for output in _test_search_request_element(package, options):
             yield output
     return 0
